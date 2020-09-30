fcd720c6e66d2f0280c60b95de1828e845f3968f
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-09-30 13:09:19 +0000

    create a new mix project

---
+++ lib/structex.ex
@@ -0,0 +1,18 @@
+defmodule Structex do
+  @moduledoc """
+  Documentation for `Structex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Structex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/structex_test.exs
@@ -0,0 +1,8 @@
+defmodule StructexTest do
+  use ExUnit.Case
+  doctest Structex
+
+  test "greets the world" do
+    assert Structex.hello() == :world
+  end
+end
