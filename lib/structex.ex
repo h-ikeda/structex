5ae08a259c50b5855e8b4aad6e7bffef77795258
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-10-02 00:59:08 +0000

    removes an unused function

--- lib/structex.ex
+++ lib/structex.ex
@@ -2,17 +2,4 @@ defmodule Structex do
   @moduledoc """
   Documentation for `Structex`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Structex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/structex_test.exs
+++ test/structex_test.exs
@@ -1,8 +1,4 @@
 defmodule StructexTest do
   use ExUnit.Case
   doctest Structex
-
-  test "greets the world" do
-    assert Structex.hello() == :world
-  end
 end
