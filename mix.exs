4949ad7f773dd5e30282dfa947c1e522a67c2609
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2021-01-02 13:40:31 +0000

    ci and publishment config

--- mix.exs
+++ mix.exs
@@ -7,7 +7,20 @@ defmodule Structex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage:
+      if System.get_env("CI") do
+        [tool: :covertool]
+      else
+        []
+      end,
+    source_url: "https://github.com/h-ikeda/structex",
+    # docs: [logo: "logo.svg"],
+    description: "Structural calculation functions.",
+    package: [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/h-ikeda/structex"}
+    ]
     ]
   end
 
@@ -25,7 +38,8 @@ defmodule Structex.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:tensorex, "~> 0.3.0"},
-      {:ex_doc, "~> 0.23", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+      {:covertool, "~> 2.0", only: [:test]}
     ]
   end
 end
