85168473c477919c60615c11007e5d7278d6e705
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2021-02-09 13:57:22 +0000

    test: coverage improvement

--- lib/structex/tensor.ex
+++ lib/structex/tensor.ex
@@ -33,7 +33,7 @@ defmodule Structex.Tensor do
             tensor: Tensorex.t() | pos_integer,
             index: %{optional(term) => {non_neg_integer, [Range.t()], pos_integer}}
           }
-  defstruct tensor: nil, index: %{}
+  defstruct [:tensor, :index]
   @behaviour Access
   @doc """
   Returns a partial tensor corresponding to the given keys.
@@ -366,6 +366,21 @@ defmodule Structex.Tensor do
                                                                                                                        [ 9, 9] => 41, [ 9, 10] => 42, [ 9, 11] => 43,
                                                                                                                        [10, 9] => 44, [10, 10] => 45, [10, 11] => 46,
                                                                                                                        [11, 9] => 47, [11, 10] => 48, [11, 11] => 49}, shape: [12, 12]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.put_key(:b, [:fixed, :fixed, :fixed])
+      ...> |> pop_in([[:a, :b]])
+      ...> |> elem(0)
+      %Tensorex{data: %{}, shape: [3, 3]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.put_key(:b, [:fixed, :fixed, :fixed])
+      ...> |> pop_in([[:a, :b]])
+      ...> |> elem(1)
+      ...> |> Structex.Tensor.assembled()
+      nil
   """
   @spec pop(t, [...]) :: {Tensorex.t(), t}
   def pop(%Structex.Tensor{tensor: %Tensorex{shape: shape} = tensor, index: index} = t, keys)
@@ -391,7 +406,7 @@ defmodule Structex.Tensor do
   Creates a new tensor.
   """
   @spec new(pos_integer) :: t
-  def new(order), do: %Structex.Tensor{tensor: order}
+  def new(order), do: %Structex.Tensor{tensor: order, index: %{}}
 
   @doc """
   Unregisters a key from the tensor.
@@ -453,21 +468,87 @@ defmodule Structex.Tensor do
                         [1, 0] => 4, [1, 1] => 5, [1, 2] => 6,
                         [2, 0] => 7, [2, 1] => 8, [2, 2] => 9,
                                                                [3, 3] => 21}, shape: [4, 4]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:free, :free, :free])
+      ...> |> Structex.Tensor.delete_key(:a)
+      ...> |> get_in([[:a, :a]])
+      nil
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:free, :free, :free])
+      ...> |> Structex.Tensor.delete_key(:a)
+      ...> |> Structex.Tensor.assembled()
+      nil
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.delete_key(:a)
+      ...> |> get_in([[:a, :a]])
+      nil
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.delete_key(:a)
+      ...> |> Structex.Tensor.assembled()
+      nil
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.put_key(:b, [:free , :free , :fixed])
+      ...> |> Structex.Tensor.delete_key(:a)
+      ...> |> get_in([[:b, :b]])
+      %Tensorex{data: %{}, shape: [3, 3]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.put_key(:b, [:free , :free , :fixed])
+      ...> |> Structex.Tensor.delete_key(:a)
+      ...> |> Structex.Tensor.assembled()
+      %Tensorex{data: %{}, shape: [2, 2]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.put_key(:b, [:free , :free , :fixed])
+      ...> |> Structex.Tensor.delete_key(:b)
+      ...> |> get_in([[:a, :a]])
+      %Tensorex{data: %{}, shape: [3, 3]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.put_key(:b, [:free , :free , :fixed])
+      ...> |> Structex.Tensor.delete_key(:b)
+      ...> |> Structex.Tensor.assembled()
+      nil
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.put_key(:b, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.delete_key(:b)
+      ...> |> get_in([[:a, :a]])
+      %Tensorex{data: %{}, shape: [3, 3]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.put_key(:b, [:fixed, :fixed, :fixed])
+      ...> |> Structex.Tensor.delete_key(:b)
+      ...> |> Structex.Tensor.assembled()
+      nil
   """
   @spec delete_key(t, term) :: t
-  def delete_key(%Structex.Tensor{tensor: %Tensorex{shape: shape}, index: index}, id)
-      when is_map_key(index, id) and map_size(index) <= 1 do
+  def delete_key(%Structex.Tensor{tensor: %Tensorex{shape: shape}, index: index}, key)
+      when is_map_key(index, key) and map_size(index) <= 1 do
     new(length(shape))
   end
 
-  def delete_key(%Structex.Tensor{tensor: order, index: index}, id)
-      when is_map_key(index, id) and map_size(index) <= 1 do
+  def delete_key(%Structex.Tensor{tensor: order, index: index}, key)
+      when is_map_key(index, key) and map_size(index) <= 1 do
     new(order)
   end
 
-  def delete_key(%Structex.Tensor{tensor: %Tensorex{shape: shape} = tensor, index: index} = t, id)
-      when is_map_key(index, id) do
-    case Map.pop(index, id) do
+  def delete_key(%Structex.Tensor{tensor: %Tensorex{shape: shape} = tensor, index: index} = t, key)
+      when is_map_key(index, key) do
+    case Map.pop(index, key) do
       {{nil, [], _}, new_index} ->
         %{t | index: new_index}
 
@@ -503,7 +584,7 @@ defmodule Structex.Tensor do
     end
   end
 
-  def delete_key(%Structex.Tensor{index: index} = t, id), do: %{t | index: Map.delete(index, id)}
+  def delete_key(%Structex.Tensor{index: index} = t, key), do: %{t | index: Map.delete(index, key)}
 
   @spec shift_index({term, {non_neg_integer, [Range.t()], pos_integer}}, integer, non_neg_integer) ::
           {term, {non_neg_integer, [Range.t()], pos_integer}}
@@ -516,30 +597,72 @@ defmodule Structex.Tensor do
   The argument `degrees` is an enumerable of `:free` or `:fixed` those represents boundary
   conditions. If `:fixed` is specified, that degree will ignore input values and the assembled
   tensor will be contracted.
+
+  When the given key already exists, the key will be overwritten. It does not conserve existing
+  values at the key.
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:free, :free , :free])
+      ...> |> Structex.Tensor.put_key(:b, [:free, :free , :free])
+      ...> |> Structex.Tensor.put_key(:c, [:free, :free , :free])
+      ...> |> put_in([[:a, :b]], Tensorex.from_list([[ 1,  2,  3], [ 4,  5,  6], [ 7,  8,  9]]))
+      ...> |> put_in([[:b, :b]], Tensorex.from_list([[11, 12, 13], [14, 15, 16], [17, 18, 19]]))
+      ...> |> put_in([[:c, :c]], Tensorex.from_list([[21, 22, 23], [24, 25, 26], [27, 28, 29]]))
+      ...> |> Structex.Tensor.put_key(:b, [:free, :fixed, :free])
+      ...> |> get_in([[:a, :b]])
+      %Tensorex{data: %{}, shape: [3, 3]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:free, :free , :free])
+      ...> |> Structex.Tensor.put_key(:b, [:free, :free , :free])
+      ...> |> Structex.Tensor.put_key(:c, [:free, :free , :free])
+      ...> |> put_in([[:a, :b]], Tensorex.from_list([[ 1,  2,  3], [ 4,  5,  6], [ 7,  8,  9]]))
+      ...> |> put_in([[:b, :b]], Tensorex.from_list([[11, 12, 13], [14, 15, 16], [17, 18, 19]]))
+      ...> |> put_in([[:c, :c]], Tensorex.from_list([[21, 22, 23], [24, 25, 26], [27, 28, 29]]))
+      ...> |> Structex.Tensor.put_key(:b, [:free, :fixed, :free])
+      ...> |> get_in([[:b, :b]])
+      %Tensorex{data: %{}, shape: [3, 3]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:free, :free , :free])
+      ...> |> Structex.Tensor.put_key(:b, [:free, :free , :free])
+      ...> |> Structex.Tensor.put_key(:c, [:free, :free , :free])
+      ...> |> put_in([[:a, :b]], Tensorex.from_list([[ 1,  2,  3], [ 4,  5,  6], [ 7,  8,  9]]))
+      ...> |> put_in([[:b, :b]], Tensorex.from_list([[11, 12, 13], [14, 15, 16], [17, 18, 19]]))
+      ...> |> put_in([[:c, :c]], Tensorex.from_list([[21, 22, 23], [24, 25, 26], [27, 28, 29]]))
+      ...> |> Structex.Tensor.put_key(:b, [:free, :fixed, :free])
+      ...> |> Structex.Tensor.assembled()
+      %Tensorex{data: %{[3, 3] => 21, [3, 4] => 22, [3, 5] => 23,
+                        [4, 3] => 24, [4, 4] => 25, [4, 5] => 26,
+                        [5, 3] => 27, [5, 4] => 28, [5, 5] => 29}, shape: [8, 8]}
+
+      iex> Structex.Tensor.new(2)
+      ...> |> Structex.Tensor.put_key(:a, [:free, :free , :invalid])
+      ** (RuntimeError) expected boundary condition to be :fixed or :free, got: :invalid
   """
   @spec put_key(t, term, Enum.t()) :: t
-  def put_key(%Structex.Tensor{index: index} = t, id, degrees) when is_map_key(index, id) do
-    put_key(delete_key(t, id), id, degrees)
+  def put_key(%Structex.Tensor{index: index} = t, key, degrees) when is_map_key(index, key) do
+    put_key(delete_key(t, key), key, degrees)
   end
 
   def put_key(
         %Structex.Tensor{tensor: %Tensorex{shape: shape} = tensor, index: index} = t,
-        id,
+        key,
         degrees
       ) do
     {new_index, range_size} = create_index(degrees, List.first(shape))
     new_shape = Enum.map(shape, &(&1 + range_size))
-    %{t | tensor: Tensorex.reshape(tensor, new_shape), index: Map.put(index, id, new_index)}
+    %{t | tensor: Tensorex.reshape(tensor, new_shape), index: Map.put(index, key, new_index)}
   end
 
-  def put_key(%Structex.Tensor{tensor: order, index: index} = t, id, degrees) do
+  def put_key(%Structex.Tensor{tensor: order, index: index} = t, key, degrees) do
     case create_index(degrees, 0) do
       {new_index, 0} ->
-        %{t | index: Map.put(index, id, new_index)}
+        %{t | index: Map.put(index, key, new_index)}
 
       {new_index, range_size} ->
         shape = List.duplicate(range_size, order)
-        %{t | tensor: Tensorex.zero(shape), index: Map.put(index, id, new_index)}
+        %{t | tensor: Tensorex.zero(shape), index: Map.put(index, key, new_index)}
     end
   end
 
